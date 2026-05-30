003f3dc40495315a70ea3541b10054f04ce97efe
Katy248 <petrovanton247@gmail.com>
2026-05-30 21:36:44 +0300

    Change something

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
-  server: {
-    proxy: {
-      // Когда ты пишешь во Vue запрос к /api, Vite перенаправит его на Go
-      "/api": {
-        target: "http://157.22.206.199:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
